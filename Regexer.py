b4e3d859797562f6dd4ec11da770fa400a23652c
Bruno Leal <bruno.leal@unesp.br>
2022-03-13 14:29:33 -0300

    rename file

--- Regexer.py
+++ Regexer.py
@@ -1,212 +1,287 @@
 from burp import IBurpExtender
-from burp import IHttpListener
-from burp import IMessageEditorController
 from burp import ITab
-from java.awt import BorderLayout
-from java.util import ArrayList
-from javax.swing import Box
+
+from java.lang import Integer
+from java.lang import Object
+from java.lang import Short
+from java.lang import String
+
+from javax.swing import BorderFactory
+from javax.swing import GroupLayout
+from javax.swing import LayoutStyle
+from javax.swing import WindowConstants
+from javax.swing import UnsupportedLookAndFeelException
+
 from javax.swing import JButton
+from javax.swing import JFrame
 from javax.swing import JLabel
 from javax.swing import JOptionPane
 from javax.swing import JPanel
 from javax.swing import JScrollPane
-from javax.swing import JSplitPane
-from javax.swing import JTabbedPane
 from javax.swing import JTable
 from javax.swing import JTextField
-from javax.swing.table import AbstractTableModel;
-from threading import Lock
+
+from javax.swing.table import DefaultTableModel
+from java.awt import Color
+from java.awt.event import MouseListener
+import sys
+try:
+    from exceptions_fix import FixBurpExceptions
+except ImportError:
+    pass
 
 
-class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController, AbstractTableModel):
-
-    # 
-    # implement IBurpExtender
-    #
+class BurpExtender(IBurpExtender, ITab):
 
     def registerExtenderCallbacks(self, callbacks):
-        # keep a reference to our callbacks object
-        self._callbacks = callbacks
 
-        # obtain an extension helpers object
-        self._helpers = callbacks.getHelpers()
+        try:
+            sys.stdout = callbacks.getStdout()
+        except:
+            pass
 
-        # define our extension name
-        callbacks.setExtensionName("Regexer")
-
-        # create the log, regex and a lock on which to synchronize when adding log entries
-        self._log = ArrayList()
-        self._regex = ArrayList()
-        self._lock = Lock()
-
-        # history tab split pane
-        splitpane = JSplitPane(JSplitPane.VERTICAL_SPLIT)
-
-        # table of log entries
-        logTable = Table(self)
-        scrollPane =  JScrollPane(logTable)
-        splitpane.setLeftComponent(scrollPane)
-
-        # tabs with request/response viewers
-        tabs = JTabbedPane()
-        self._requestViewer = callbacks.createMessageEditor(self, False)
-        self._responseViewer = callbacks.createMessageEditor(self, False)
-        tabs.addTab("Request", self._requestViewer.getComponent())
-        tabs.addTab("Response", self._responseViewer.getComponent())
-        splitpane.setRightComponent(tabs)
-        
-        # regex tab panel
-        panel = JPanel(BorderLayout())
-        
-        box = Box.createHorizontalBox() 
-        box.add(JLabel("Regular Expression: "))
-        box.add(JTextField(100, actionPerformed=self.updateRegex))
-        box.add(JButton("Update", actionPerformed=self.updateRegex))
-        box.add(JButton("?", actionPerformed=self.help))
-        
-        panel.add(box, BorderLayout.NORTH)
-
-        box = Box.createHorizontalBox() 
-        self._regex.add([1, "Issue1", "Severity1"])
-        self._regex.add([2, "Issue2", "Severity2"])
-        self._regex.add([3, "Issue3", "Severity3"])
-        self._tableHeadings = ["#", "Rule Name", "Regular Expression"]
-        self._regexTable = JTable(self._regex, self._tableHeadings)
-        regexScrollPane = JScrollPane(self._regexTable)
-        
-        panel.add(regexScrollPane, BorderLayout.CENTER)
-
-        # main tabs 
-        self._tabs = JTabbedPane()
-        self._tabs.addTab("History", splitpane)
-        self._tabs.addTab("Regex", panel)
-
-        # customize our UI components
-        callbacks.customizeUiComponent(self._tabs)
-        callbacks.customizeUiComponent(splitpane)
-        callbacks.customizeUiComponent(logTable)
-        callbacks.customizeUiComponent(scrollPane)
-        callbacks.customizeUiComponent(tabs) 
-
-        # add the custom tab to Burp's UI
+        callbacks.setExtensionName("RegexerGUI")
         callbacks.addSuiteTab(self)
 
-        # register ourselves as an HTTP listener
-        callbacks.registerHttpListener(self)
-
-        return
-
-    #
-    # implement ITab
-    #
-
     def getTabCaption(self):
-        return "Regexer"
+        return "RegexerGUI"
 
     def getUiComponent(self):
-        return self._tabs
-
-    #
-    # implement ITextEditor
-    #
-
-
-    #
-    # implement IHttpListener
-    #
-
-    def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
-        # only process requests
-        if messageIsRequest:
-            return
-
-        # create a new log entry with the message details
-        self._lock.acquire()
-        row = self._log.size()
-        self._log.add(LogEntry(toolFlag, self._callbacks.saveBuffersToTempFiles(messageInfo), self._helpers.analyzeRequest(messageInfo).getUrl()))
-        self.fireTableRowsInserted(row, row)
-        self._lock.release()
-
-    #
-    # extend AbstractTableModel
-    #
-
-    def getRowCount(self):
-        try:
-            return self._log.size()
-        except:
-            return 0
-
-    def getColumnCount(self):
-        return 2
-
-    def getColumnName(self, columnIndex):
-        if columnIndex == 0:
-            return "Tool"
-        if columnIndex == 1:
-            return "URL"
-        return ""
-
-    def getValueAt(self, rowIndex, columnIndex):
-        logEntry = self._log.get(rowIndex)
-        if columnIndex == 0:
-            return self._callbacks.getToolName(logEntry._tool)
-        if columnIndex == 1:
-            return logEntry._url.toString()
-        return ""
-
-    #
-    # auxiliary methods
-    #
-
-    def updateRegex(self, event):
-        print("UPDATE REGEX")
-        self._regex.add([4, "Issue4", "Severity4"])
-        print(self._regex)
-
-    def help(self, event):
-        JOptionPane.showMessageDialog(None, "All matching subgroups will also be extracted.")
-
-    # 
-    # implement IMessageEditorController
-    # this allows our request/response viewers to obtain details about the messages being displayed 
-    #
-
-    def getHttpService(self):
-        return self._currentlyDisplayedItem.getHttpService()
-
-    def getRequest(self):
-        return self._currentlyDisplayedItem.getRequest()
-
-    def getResponse(self):
-        return self._currentlyDisplayedItem.getResponse()
-
-    
-#
-# extend JTable to handle cell selection
-#
-
-class Table(JTable):
-    def __init__(self, extender):
-        self._extender = extender
-        self.setModel(extender)
-
-    def changeSelection(self, row, col, toggle, extend):
-        # show the log entry for the selected row
-        logEntry = self._extender._log.get(row)
-        self._extender._requestViewer.setMessage(logEntry._requestResponse.getRequest(), True)
-        self._extender._responseViewer.setMessage(logEntry._requestResponse.getResponse(), True)
-        self._extender._currentlyDisplayedItem = logEntry._requestResponse
-
-        JTable.changeSelection(self, row, col, toggle, extend)
+        tableData = [
+            [1, "1st rule", "://"],
+            [2, "2nd rule", "url="],
+            [3, "3rd rule", "<a link="]
+        ]
+        tableColumns = ["#", "Rule Name", "Regex Rule"]
+        
+        regexerGui = RegexerGUI(tableData, tableColumns)
+        # regexerGuiEdit = RegexerGUIEdit()
+        # regexerGuiEdit.pack()
+        # regexerGuiEdit.show()
+        return regexerGui.panel
 
 
-#
-# class to hold details of each log entry
-#
+class RegexerGUI(JFrame):
 
-class LogEntry:
-    def __init__(self, tool, requestResponse, url):
-        self._tool = tool
-        self._requestResponse = requestResponse
-        self._url = url
+    def __init__(self, data, columns):
+        self.jScrollPane1 = JScrollPane()
+        self.jTableRegex = JTable()
+        self.jButtonAdd = JButton("Add", actionPerformed=self.handleJButtonAdd)
+        self.jButtonRemove = JButton("Remove", actionPerformed=self.handleJButtonRemove)
+        self.jButtonEdit = JButton("Edit", actionPerformed=self.handleJButtonEdit)
+
+        self.jTableRegex = RegexTable(data, columns)
+        self.jScrollPane1.setViewportView(self.jTableRegex)
+
+        self.panel = JPanel()
+        layout = GroupLayout(self.panel)
+        self.panel.setLayout(layout)
+        layout.setAutoCreateGaps(True)
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING, False)
+                    .addComponent(self.jButtonEdit, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(self.jButtonAdd, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(self.jButtonRemove, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(self.jScrollPane1, GroupLayout.DEFAULT_SIZE, 777, Short.MAX_VALUE))
+        )
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(self.jScrollPane1, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(self.jButtonAdd, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+                        .addGap(4, 4, 4)
+                        .addComponent(self.jButtonEdit, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(self.jButtonRemove, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 182, Short.MAX_VALUE))))
+        )             
+
+    def handleJButtonAdd(self, event):
+        regexerGUIEdit = RegexerGUIEdit(self.jTableRegex, event)
+        regexerGUIEdit.pack()
+        regexerGUIEdit.show()
+
+    def handleJButtonEdit(self, event):
+        regexerGUIEdit = RegexerGUIEdit(self.jTableRegex, event)
+        regexerGUIEdit.pack()
+        regexerGUIEdit.show()
+
+    def handleJButtonRemove(self, event):
+        if(self.jTableRegex.getSelectedRow() != -1):
+            self.jTableRegex.removeRow(self.jTableRegex.getSelectedRow())
+            JOptionPane.showMessageDialog(None, "Selected row deleted successfully")
+
+
+class RegexerGUIEdit(JFrame):
+
+    def __init__(self, jTableRegex, event):
+        self.jTableRegex = jTableRegex
+        self._event = event
+        
+        self.jLabel1 = JLabel()
+        self.jLabel1.setText("Specify the details of the regex rule.")
+        
+        self.jLabel2 = JLabel()
+        self.jLabel2.setText("Rule Name:")
+        
+        self.jLabel3 = JLabel()
+        self.jLabel3.setText("Regex:")
+
+        self.jTextFieldRuleName = JTextField()
+        self.jTextFieldRegex = JTextField()
+
+        if event.source.text == "Add":
+            self.setTitle("Add Regex Rule")
+        elif event.source.text == "Edit":
+            self.setTitle("Edit Regex Rule")
+            self.jTextFieldRuleName.setText(self.jTableRegex.getValueAt(self.jTableRegex.getSelectedRow(), 1))
+            self.jTextFieldRegex.setText(self.jTableRegex.getValueAt(self.jTableRegex.getSelectedRow(), 2))
+        
+        self.jButtonOK = JButton("OK", actionPerformed=self.addEditRegex)
+        self.jButtonCancel = JButton("Cancel", actionPerformed=self.closeRegexerGUIEdit)
+
+        self.jTextFieldRuleName.setToolTipText("")
+        self.jTextFieldRuleName.setBorder(BorderFactory.createLineBorder(Color.lightGray));
+        self.jTextFieldRegex.setToolTipText("")
+        self.jTextFieldRegex.setBorder(BorderFactory.createLineBorder(Color.lightGray));
+        
+        layout = GroupLayout(self.getContentPane())
+        self.getContentPane().setLayout(layout)
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(self.jLabel1)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(self.jButtonOK, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(self.jButtonCancel, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(self.jLabel2)
+                                .addComponent(self.jLabel3))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, False)
+                                .addComponent(self.jTextFieldRuleName)
+                                .addComponent(self.jTextFieldRegex, GroupLayout.DEFAULT_SIZE, 382, Short.MAX_VALUE)))))
+                .addContainerGap(20, Short.MAX_VALUE))
+        )
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(self.jLabel1)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(self.jLabel2)
+                    .addComponent(self.jTextFieldRuleName, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(self.jLabel3)
+                    .addComponent(self.jTextFieldRegex, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                    .addComponent(self.jButtonCancel, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(self.jButtonOK, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        )
+
+    def addEditRegex(self, event):
+        if self._event.source.text == "Add":
+            lastIndex = self.jTableRegex.getValueAt(self.jTableRegex.getRowCount()-1, 0)
+            self.jTableRegex.addRow([lastIndex + 1, self.jTextFieldRuleName.getText(), self.jTextFieldRegex.getText()])
+            print(self.jTableRegex.getModel().getDataVector())
+            self.dispose()
+        elif self._event.source.text == "Edit":
+            rowIndex = self.jTableRegex.getSelectedRow()
+            self.jTableRegex.setValueAt(self.jTextFieldRuleName.getText(), rowIndex, 1) # Rule Name column
+            self.jTableRegex.setValueAt(self.jTextFieldRegex.getText(), rowIndex, 2) # Regex Rule column
+            # self.jTableRegex.getModel().fireTableDataChanged()
+            print(self.jTableRegex.getModel().getDataVector())
+            self.dispose()
+
+
+    def closeRegexerGUIEdit(self, event):
+        self.dispose()
+
+
+class RegexTableModel(DefaultTableModel):
+
+    def __init__(self, data, columnNames):
+        DefaultTableModel.__init__(self, data, columnNames)
+
+    def isCellEditable(self, row, column):
+        canEdit = [False, False, False]
+        return canEdit[column]
+
+    def getColumnClass(self, column):
+        columnClasses = [Integer, String, String]
+        return columnClasses[column]
+
+
+class RegexTableMouseListener(MouseListener):
+
+    def getClickedIndex(self, event):
+        eventTable = event.getSource()
+        row = eventTable.getSelectedRow()
+        return eventTable.getValueAt(row, 0)
+
+    def getClickedRow(self, event):
+        eventTable = event.getSource()
+        return eventTable.getModel().getDataVector().elementAt(eventTable.getSelectedRow())
+
+    def mouseClicked(self, event):
+        if event.getClickCount() == 1:
+            print("Single-click: {}".format(self.getClickedRow(event)))
+        elif event.getClickCount() == 2:
+            print("Double-click: {}".format(self.getClickedRow(event)))
+            tbl = event.getSource()
+            tbl.addRow([11, "dblclick-name", "dblclick-severity"])
+        else:
+            print("Another element: {}".format(event))
+
+    def mouseEntered(self, event):
+        pass
+
+    def mouseExited(self, event):
+        pass
+
+    def mousePressed(self, event):
+        pass
+
+    def mouseReleased(self, event):
+        pass
+
+
+class RegexTable(JTable):
+
+    def __init__(self, data, columnNames):
+        model = RegexTableModel(data, columnNames)
+        self.setModel(model)
+        self.setAutoCreateRowSorter(True)
+        self.getTableHeader().setReorderingAllowed(False)
+        self.addMouseListener(RegexTableMouseListener())
+
+    def addRow(self, data):
+        self.getModel().addRow(data)
+
+    def removeRow(self, row):
+        self.getModel().removeRow(row)
+
+    def setValueAt(self, value, row, column):
+        self.getModel().setValueAt(value, row, column)
+
+
+# support for burp-exceptions
+try:
+    FixBurpExceptions()
+except:
+    pass
